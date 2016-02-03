13c6db2a09615fd4a306eb56113a84ba1c1d4a66
Jonathan Gray <jhg03a@acu.edu>
2016-02-02 19:26:45 -0600

    Fixed bug where the environment wasn't being set properly when being called from facter.

--- lib/facter/agentfacts.rb
+++ lib/facter/agentfacts.rb
@@ -1,5 +1,10 @@
 require 'facter'
 
+# Set the preferred run mode.  This dictates what config sections get loaded.
+# This fixes the issue where you are getting agentfacts from a manual facter -p run
+# and the environment setting is wrong.
+Puppet.settings.preferred_run_mode= :agent
+
 # Pull in puppet settings from global variable
 settings = Puppet.settings.to_h
 
